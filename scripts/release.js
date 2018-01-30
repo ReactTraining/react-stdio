6f4115282a8243184428876b227570143746fa3c
MICHAEL JACKSON <mjijackson@gmail.com>
2018-01-30 13:02:12 -0800

    Update release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,17 +1,15 @@
-const resolvePath = require("path").resolve;
-const readFileSync = require("fs").readFileSync;
+const fs = require("fs");
+const path = require("path");
 const execSync = require("child_process").execSync;
 const prompt = require("readline-sync").question;
+
+process.chdir(path.resolve(__dirname, ".."));
 
 const exec = command => execSync(command, { stdio: "inherit" });
 
 const getPackageVersion = () =>
-  JSON.parse(readFileSync(resolvePath(__dirname, "../package.json"))).version;
-
-if (process.cwd() !== resolvePath(__dirname, "..")) {
-  console.error("The release script must be run from the repo root");
-  process.exit(1);
-}
+  JSON.parse(fs.readFileSync(path.resolve(__dirname, "../package.json")))
+    .version;
 
 // Get the next version, which may be specified as a semver
 // version number or anything `npm version` recognizes. This
@@ -20,16 +18,16 @@ if (process.cwd() !== resolvePath(__dirname, "..")) {
 const nextVersion = prompt(
   `Next version (current version is ${getPackageVersion()})? `
 );
-const isPrerelease = nextVersion.substring(0, 3) === "pre";
+const isPrerelease =
+  nextVersion.substr(0, 3) === "pre" || nextVersion.indexOf("-") !== -1;
 
-// 1) Increment the package version in package.json
-// 2) Create a new commit
-// 3) Create a v* tag that points to that commit
+// 1) Make sure the tests pass
+exec("npm test");
+
+// 2) Increment the package version in package.json
+// 3) Create a new commit
+// 4) Create a v* tag that points to that commit
 exec(`npm version ${nextVersion} -m "Version %s"`);
-
-// 4) Push to GitHub master. Do this before we publish in
-// case anyone has pushed to GitHub since we last pulled
-exec("git push origin master");
 
 // 5) Publish to npm. Use the "next" tag for pre-releases,
 // "latest" for all others
@@ -37,9 +35,3 @@ exec(`npm publish --tag ${isPrerelease ? "next" : "latest"}`);
 
 // 6) Push the v* tag to GitHub
 exec(`git push -f origin v${getPackageVersion()}`);
-
-// 7) Push the "latest" tag to GitHub
-if (!isPrerelease) {
-  exec("git tag -f latest");
-  exec("git push -f origin latest");
-}
