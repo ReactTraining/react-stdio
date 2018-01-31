c75e1386ebb0ebcb38099a5553b2c40ac5f94e7b
MICHAEL JACKSON <mjijackson@gmail.com>
2018-01-31 11:22:05 -0800

    Deploy to npm from Travis

---
+++ scripts/build.js
@@ -0,0 +1,13 @@
+const execSync = require("child_process").execSync;
+
+const tag = process.env.TRAVIS_TAG || `v${process.env.npm_package_version}`
+const platforms = ["win-x64", "win-x86", "linux-x64", "linux-x86", "macos"];
+
+platforms.forEach(platform => {
+  console.log(`\nBuilding binary for ${platform}...`);
+
+  execSync(
+    `pkg ./bin/react-stdio -t ${platform} -o ./build/${tag}/${platform}/react-stdio`,
+    { stdio: "inherit" }
+  );
+});
