21df0a9d0b8bba182232fcdc0f9fcb88ab17b017
MICHAEL JACKSON <mjijackson@gmail.com>
2018-01-31 13:55:34 -0800

    Disable x86 binaries for now

--- scripts/build.js
+++ scripts/build.js
@@ -1,11 +1,16 @@
 const execSync = require("child_process").execSync;
 
-const tag = process.env.TRAVIS_TAG || `v${process.env.npm_package_version}`
-const platforms = ["win-x64", "win-x86", "linux-x64", "linux-x86", "macos"];
+const tag = process.env.TRAVIS_TAG || `v${process.env.npm_package_version}`;
+const platforms = [
+  "win-x64",
+  // "win-x86", // pending https://github.com/zeit/pkg/issues/310
+  "linux-x64",
+  // "linux-x86", // pending https://github.com/zeit/pkg/issues/310
+  "macos"
+];
 
 platforms.forEach(platform => {
   console.log(`\nBuilding binary for ${platform}...`);
-
   execSync(
     `pkg ./bin/react-stdio -t ${platform} -o ./build/${tag}/${platform}/react-stdio`,
     { stdio: "inherit" }
